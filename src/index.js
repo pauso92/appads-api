1f128440246767f7b81ebbc52e2218e50706ae03
Yoeli <yoeli.cruz20@gmail.com>
2020-02-20 21:38:09 -0600

    [add] dotenv and jwt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const { ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = require('./schema')
@@ -6,4 +7,7 @@ const resolvers = require('./resolvers');
 const server = new ApolloServer({ typeDefs, resolvers })
 server.listen().then(({ url }) => {
   console.log(`🎸 server ready at ${url}`)
+
 })
+
+
