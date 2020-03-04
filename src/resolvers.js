259f99ae87d52f92e55ad62ce63886c4df890c2f
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-03-03 19:38:14 -0600

    [add] chart data example

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,8 +1,10 @@
 import books from './books.json'
+import charts from './chartdata.json'
 
 const resolvers = {
   Query: {
     books: () => books,
+    getChartData: () => charts,
     login: async (_, { email, password }, { dataSources }) =>
       await dataSources.authAPI.getToken({ email, password }),
     getPosts: async (_, __, { dataSources, token }) =>
--- src/schema.js
+++ src/schema.js
@@ -1,18 +1,29 @@
 import { gql } from 'apollo-server'
 
 const typeDefs = gql`
-  type Book{
-    title: String
-    author: String
-  }
-  type Query{
+  type Query {
     books:[Book]
+    getChartData: [ChartJs]
     login(email:String, password: String): String
     getPosts: [Post]
+  }
+  type Book {
+    title: String
+    author: String
   }
   type Post {
     author: String
     comment: String
+  }
+  type ChartJs {
+    type: String,
+    data: [ChartJsLinerPointData],
+    options: String
+    # options: [ChartJsOptions]
+  }
+  type ChartJsLinerPointData {
+    x: Int,
+    y: Int
   }
 `
 module.exports = typeDefs;
