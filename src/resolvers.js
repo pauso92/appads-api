fd9c3aa58704ef91f4d1a1fdd43b908338eed778
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-03-19 15:34:29 -0600

    [add] verifyToken to books query

--- src/resolvers.js
+++ src/resolvers.js
@@ -3,7 +3,9 @@ import charts from './chartdata.json'
 
 const resolvers = {
   Query: {
-    books: () => books,
+    books: async (_, __, { dataSources, token }) =>
+      await dataSources.authAPI.verifyToken(token)
+      && books,
     getChartData: () => charts,
     login: async (_, { email, password }, { dataSources }) =>
       await dataSources.authAPI.getToken({ email, password }),
